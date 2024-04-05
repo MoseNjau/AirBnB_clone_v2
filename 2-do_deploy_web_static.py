5bd8134556bbfcb2f3880d3780c0c1822187158f
MoseNjau <moses.njau@students.jkuat.ac.ke>
2024-04-05 23:28:37 +0300

    modify file 3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,26 +1,46 @@
 #!/usr/bin/python3
-"""Fabric script that distributes an archive to your web servers,
-using the function do_deploy."""
-from fabric.api import *
-from os import path
-env.hosts = ['54.237.92.118', '100.25.17.250']
+"""
+Fabric script to deploy tgz archive
+fab -f 2-do_deploy_web_static.py do_deploy:archive_path=filepath
+    -i private-key -u user
+"""
+
+from os.path import exists
+from fabric.api import put, run, env
+
+env.hosts = ['35.243.128.200', '3.239.120.96']
 
 
 def do_deploy(archive_path):
-    """Function to deploy"""
-    if not path.exists(archive_path):
+    """
+    copies archive file from local to my webservers
+    """
+
+    if not exists(archive_path):
         return False
     try:
+        file_name = archive_path.split("/")[-1].split(".")[0]
         put(archive_path, "/tmp/")
-        name_file = archive_path.split("/")[-1]
-        name_folder = ("/data/web_static/releases/" + name_file.split(".")[0])
-        run("mkdir -p {}".format(name_folder))
-        run("tar -xzf /tmp/{} -C {}".format(name_file, name_folder))
-        run("rm /tmp/{}".format(name_file))
-        run("mv {}/web_static/* {}".format(name_folder, name_folder))
-        run("rm -rf {}/web_static".format(name_folder))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(name_folder))
+
+        run("mkdir -p /data/web_static/releases/{}".format(file_name))
+
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+            .format(file_name, file_name))
+
+        run('rm -rf /tmp/{}.tgz'.format(file_name))
+
+        run(('mv /data/web_static/releases/{}/web_static/* ' +
+            '/data/web_static/releases/{}/')
+            .format(file_name, file_name))
+
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_name))
+
+        run('rm -rf /data/web_static/current')
+
+        run(('ln -s /data/web_static/releases/{}/' +
+            ' /data/web_static/current')
+            .format(file_name))
         return True
-    except:
+    except Exception:
         return False
